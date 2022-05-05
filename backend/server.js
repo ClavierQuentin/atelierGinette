c07faa278bc6cdb443e2353926e56c92dc97d8a1
quentin <clavier.quentin@gmail.com>
2022-05-05 13:28:07 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -72,7 +72,8 @@ app.get("/pages/about", (req,res) => {
         res.send(article)
     })
 })
-app.use(express.static(path.join(__dirname, '../frontend')));
+//app.use(express.static(path.join(__dirname, '../frontend')));
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/index.html'));
   });
