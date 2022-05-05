a11899bb44dafe6d2010932b292ff753ba791841
quentin <clavier.quentin@gmail.com>
2022-05-05 13:44:12 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -72,11 +72,10 @@ app.get("/pages/about", (req,res) => {
         res.send(article)
     })
 })
-//app.use(express.static(path.join(__dirname, '../frontend')));
 
-app.get('*', (req, res) => {
+/*app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/index.html'));
-  });
+  });*/
 /*app.listen(5000, () => {
     console.log('serveur sur http://localhost:5000');
 })*/
