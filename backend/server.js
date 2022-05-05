5f09e711584eac54a6c4766810926355774d2c70
quentin <clavier.quentin@gmail.com>
2022-05-05 13:09:44 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -72,9 +72,9 @@ app.get("/pages/about", (req,res) => {
         res.send(article)
     })
 })
-app.use(express.static(path.join(__dirname, '/../frontend')));
+app.use(express.static(path.join(__dirname, '../frontend')));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/../frontend/index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
   });
 /*app.listen(5000, () => {
     console.log('serveur sur http://localhost:5000');
