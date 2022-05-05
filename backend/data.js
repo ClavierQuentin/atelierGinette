4539063af5bb983331b62516ea64e299365d1196
quentin <clavier.quentin@gmail.com>
2022-05-05 19:16:20 +0200

    commit

--- backend/data.js
+++ backend/data.js
@@ -19,180 +19,134 @@ module.exports ={
     ],
  photosPrez :  [
         {
-            photo: "../img/prez/atelier-ginette-créations-couture-artisanat.jpg",
+            url_image: "../img/prez/Atelier ginette artisanat couture fait main.jpg",
             id: 1
         },
         {
-            photo: "../img/prez/IMG_20210719_163446_720_161904234804461.jpg",
+            url_image: "../img/prez/atelier-ginette-artisanat-couture.jpg",
             id: 2
         },
         {
-            photo: "../img/prez/DSCF1894.jpg",
+            url_image: "../img/prez/atelier-ginette-créations-couture-artisanat.jpg",
             id: 3
         },
         {
-            photo: "../img/prez/créations coutures - artisanat - atelier ginette.jpg",
+            url_image: "../img/prez/créations coutures - artisanat - atelier ginette.jpg",
             id: 4
         },
         {
-            photo: "../img/prez/DSCF7904.jpg",
+            url_image: "../img/prez/DSCF1894.jpg",
             id: 5
+        },
+        {
+            url_image: "../img/prez/DSCF7904.jpg",
+            id: 6
+        },
+        {
+            url_image: "../img/prez/DSCF7904.jpg",
+            id: 7
+        },
+        {
+            url_image: "../img/prez/DSCF9966.jpg",
+            id: 8
+        },
+        {
+            url_image: "../img/prez/IMG_20210719_163446_720_161904234804461.jpg",
+            id: 9
         }
     ],
 
  listeCategories : [
     {
-        id: 1,
-        categorie : "Coussinets de lavande",
-        urlImageCategorie : "../img/coussinets/DSCF0081.jpg",
-        produit :[
-            {
-                nom: "Coussinet de lavande",
-                urlImgProduit : "../img/coussinets/DSCF0030.jpg",
-                prix: "15,00"
-            },
-            {
-                nom: "Coussinet de lavande",
-                urlImgProduit : "../img/coussinets/DSCF0078.jpg",
-                prix: "15,00"
-            },
-            {
-                nom: "Coussinet de lavande",
-                urlImgProduit : "../img/coussinets/DSCF0081.jpg",
-                prix: "15,00"
-            }
-        ]
-
+        id_categorie: 1,
+        nom_categorie : "Coussinets de lavande",
+        url_img : "../img/coussinets/DSCF0081.jpg"
     },
     {
-        id: 2,
-        categorie : "Pochettes à savon",
-        urlImageCategorie : "../img/pochette-savon-solide-atelier-ginette-4.jpg",
-        produit :[
-            {
-                nom: "Pochette à savon solide en tissu nid d'abeille",
-                urlImgProduit : "../img/pochetteSavon/pochette-savon-solide-atelier-ginette-42.jpg",
-                prix: "15,00"
-            },
-            {
-                nom: "Pochette à savon solide en tissu nid d'abeille",
-                urlImgProduit : "../img/pochetteSavon/pochette-savon-solide-atelier-ginette-35.jpg",
-                prix: "15,00"
-            },
-            {
-                nom: "Pochette à savon solide en tissu nid d'abeille",
-                urlImgProduit : "../img/pochetteSavon/pochette-savon-solide-atelier-ginette-58.jpg",
-                prix: "15,00"
-            },
-            {
-                nom: "Pochette à savon solide en tissu nid d'abeille",
-                urlImgProduit : "../img/pochetteSavon/DSCF0486.jpg",
-                prix: "15,00"
-            }
-        ]
-
-
+        id_categorie: 2,
+        nom_categorie : "Pochettes à savon",
+        url_img : "../img/pochetteSavon/pochette-savon-solide-atelier-ginette-4.jpg"
     },
     {
-        id: 3,
-        categorie : "Trousses de toilette",
-        urlImageCategorie : "../img/trousse-maquillage-artisanat-atelier-ginette-1.jpg",
-        produit :[
-            {
-                nom: "Trousse de toilette berlingot matelassée",
-                urlImgProduit : "../img/DSCF0366.jpg",
-                prix: "25,00"
-            },
-            {
-                nom: "Trousse de toilette berlingot matelassée",
-                urlImgProduit : "../img/trousse-maquillage-artisanat-atelier-ginette-1.jpg",
-                prix: "25,00"
-            },
-            {
-                nom: "Trousse de toilette berlingot matelassée",
-                urlImgProduit : "../img/DSCF0352.jpg",
-                prix: "25,00"
-            }
-        ]
+        id_categorie: 3,
+        nom_categorie : "Trousses de toilette",
+        url_img : "../img/trousse-maquillage-artisanat-atelier-ginette-1.jpg"
     },
     {
-        id: 4,
-        categorie : "Cotons démaquillants",
-        urlImageCategorie : "../img/cotons-lavables-bambou-atelier-ginette-1.jpg",
-        produit :[
-            {
-                nom: "Cotons démaquillants en bambou, cotons lavables zéro déchet certifiés Oeko-Tex ",
-                urlImgProduit : "../img/cotons/DSCF0336.jpg",
-                prix: "18,30"
-            },
-            {
-                nom: "Cotons démaquillants en bambou, cotons lavables zéro déchet certifiés Oeko-Tex ",
-                urlImgProduit : "../img/cotons/cotons-lavables-bambou-atelier-ginette-1.jpg",
-                prix: "18,30"
-            },
-            {
-                nom: "Cotons démaquillants en bambou, cotons lavables zéro déchet certifiés Oeko-Tex ",
-                urlImgProduit : "../img/cotons/créations-couture-artisanat-atelier-ginette-1.jpg",
-                prix: "18,30"
-            }
-        ]
+        id_categorie: 4,
+        nom_categorie : "Cotons démaquillants",
+        url_img : "../img/cotons-lavables-bambou-atelier-ginette-1.jpg"
     },
     {
-        id: 5,
-        categorie : "Portes-monnaies",
-        urlImageCategorie : "../img/DSCF9334.jpg"
+        id_categorie: 5,
+        nom_categorie : "Portes-monnaies",
+        url_img : "../img/DSCF9334.jpg"
     },
     {
-        id: 6,
-        categorie : "Guirlandes de fanions",
-        urlImageCategorie : "../img/DSCF0018.jpg"
+        id_categorie: 6,
+        nom_categorie : "Guirlandes de fanions",
+        url_img : "../img/DSCF0018.jpg"
     },
     {
-        id: 7,
-        categorie : "Trousses molletonnées",
-        urlImageCategorie : "../img/DSCF9915.jpg"
+        id_categorie: 7,
+        nom_categorie : "Trousses molletonnées",
+        url_img : "../img/DSCF9915.jpg"
     },
     {
-        id: 8,
-        categorie : "Totes bags",
-        urlImageCategorie : "../img/DSCF9443.jpg"
+        id_categorie: 8,
+        nom_categorie : "Totes bags",
+        url_img : "../img/DSCF9443.jpg"
     }
 ],
 
  listeProduits : [
     {
-        idCategorie : 3,
-        nom: "Trousse de toilette berlingot matelassée",
-        urlImgProduit : "../img/DSCF0366.jpg",
-        prix: 25
+        id_produit : 1,
+        nom_produit: "Pochette à savon solide en tissu nid d'abeille",
+        description_courte : `<p>Pochette à savon solide en tissu d'abeille</p>
+        <br>
+        <p>Idéal pour ranger vos savons solides au quotidien ou pour les transporter lors de vos petites ou grandes aventures et les protéger.</p>
+        <br>
+        <p>Le tissu nid d'abeille est très absorbant et gardera votre savonnette au sec !
+        Fabriqué à la main dans le Bocage Normand.</p>`,
+        description_longue: `<p>COMPOSITION<br>
+        Tissu 100 % coton.<br>
+        A laver à la main délicatement.<br>
+        Pas de sèche-linge.<br></p>
+        <p>DIMENSIONS<br>
+        Les pochettes à savon solide font environ 11 cm de hauteur et 12 cm de largeur.<br>
+        A noter que tous les produits de l'Atelier Ginette sont artisanaux, tout est fabriqué à la main, les dimensions peuvent donc varier légèrement.</p>
+        <p>DECLINAISONS<br>
+        Ce modèle existe en plusieurs coloris.</p> `,
+        url_image:"../img/pochetteSavon/DSCF0477.jpg",
+        prix_produit: 13,
+        stock_produit: 1,
+        id_categorie:2,
+        nom_categorie: "Pochettes à savon"
     },
     {
-        idCategorie : 3,
-        nom: "Trousse de toilette berlingot matelassée",
-        urlImgProduit : "../img/DSCF0366.jpg",
-        prix: 25
-    },
-    {
-        idCategorie : 3,
-        nom: "Trousse de toilette berlingot matelassée",
-        urlImgProduit : "../img/DSCF0366.jpg",
-        prix: 25
-    },
-    {
-        idCategorie : 4,
-        nom: "Trousse de toilette berlingot matelassée",
-        urlImgProduit : "../img/DSCF0366.jpg",
-        prix: 25
-    },{
-        idCategorie : 4,
-        nom: "Trousse de toilette berlingot matelassée",
-        urlImgProduit : "../img/DSCF0366.jpg",
-        prix: 25
-    },{
-        idCategorie : 4,
-        nom: "Trousse de toilette berlingot matelassée",
-        urlImgProduit : "../img/DSCF0366.jpg",
-        prix: 25
+        id_produit : 1,
+        nom_produit: "Pochette à savon solide en tissu nid d'abeille",
+        description_courte : `<p>Pochette à savon solide en tissu d'abeille</p>
+        <br>
+        <p>Idéal pour ranger vos savons solides au quotidien ou pour les transporter lors de vos petites ou grandes aventures et les protéger.</p>
+        <br>
+        <p>Le tissu nid d'abeille est très absorbant et gardera votre savonnette au sec !
+        Fabriqué à la main dans le Bocage Normand.</p>`,
+        description_longue: `<p>COMPOSITION<br>
+        Tissu 100 % coton.<br>
+        A laver à la main délicatement.<br>
+        Pas de sèche-linge.<br></p>
+        <p>DIMENSIONS<br>
+        Les pochettes à savon solide font environ 11 cm de hauteur et 12 cm de largeur.<br>
+        A noter que tous les produits de l'Atelier Ginette sont artisanaux, tout est fabriqué à la main, les dimensions peuvent donc varier légèrement.</p>
+        <p>DECLINAISONS<br>
+        Ce modèle existe en plusieurs coloris.</p> `,
+        url_image:"../img/pochetteSavon/DSCF0477.jpg",
+        prix_produit: 13,
+        stock_produit: 1,
+        id_categorie:2,
+        nom_categorie: "Pochettes à savon"    
     }
 ]
 }
--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,39 @@ const app = express()
 const mysql = require('mysql')
 const { type, json, jsonp } = require('express/lib/response');
 const path = require('path');
+const { request } = require('http')
 
 
 app.use(cors())
 
+app.get('/api/produit/', (req,res) => {
+    res.send(data.listeCategories);
+})
+app.get("/pages/categories/:id", (req, res) => {
+    let response = []
+    for(let i = 0; i < data.listeProduits.length; i++){
+        if(req.params.id == data.listeProduits[i].id_categorie){
+            response.push(data.listeProduits[i]);
+        }
+    }
+    res.send(response) 
+})
+app.get("/pages/produit/:id", (req,res) => {
+    let response = []
+    for(let y = 0; y<data.listeProduits.length; y++){
+        if(req.params.id == data.listeProduits[y].id_produit){
+            response.push(data.listeProduits[y]);
+        }
+    }
+    res.send(response)
+})
+app.get("/pages/about", (req,res) => {
+    res.send(data.photosPrez)
+})
 //Connexion
 /*
 const db = mysql.createConnection({
-    host: '185.98.131.176',
+    host: 'mysql22.lwspanel.com',
     user: 'genie1829577_12bkj',
     password: 'Dwwm2022@1984',
     database: 'genie1829577_12bkj'
@@ -22,7 +47,7 @@ db.connect((err) =>{
         throw err
     }
     console.log('Connecté à MySql');
-})*/
+})
  
 
 
@@ -76,9 +101,9 @@ app.get("/pages/about", (req,res) => {
 app.use(express.static(path.join(__dirname, '../frontend')));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/index.html'));
-  });
-/*app.listen(5000, () => {
+  });*/
+app.listen(5000, () => {
     console.log('serveur sur http://localhost:5000');
-})*/
+})
 
-app.listen(process.env.PORT);
+/*app.listen(process.env.PORT);*/
