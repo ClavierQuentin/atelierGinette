ff3abeec46a3d64fa90ca46492bb5ca1f76581d5
quentin <clavier.quentin@gmail.com>
2022-06-17 13:50:14 +0200

    gignore

--- backend/dbInfo.js
+++
@@ -1,9 +0,0 @@
-
-exports.dbInfo = {
-    host: 'tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user: 'gcce05r9brilm3jj',
-    password: 'y8pr8u3iyjk597y9',
-    database: 'zlsorffc7qeih1l3',
-    charset  : 'UTF8_UNICODE_CI',
-    multipleStatements: true
-};
